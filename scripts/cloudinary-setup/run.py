3bb4541eccdc709e6565f8466c9749da14040059
Justin Hoang <jnhustin@gmail.com>
2021-12-13 20:45:02 -0800

    establish outline of main

--- scripts/cloudinary-setup/run.py
+++ scripts/cloudinary-setup/run.py
@@ -1,11 +1,14 @@
 import os
+import json
+from os.path import join, dirname
+from typing import List
 
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 
+
 from dotenv  import load_dotenv
-from os.path import join, dirname
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -17,16 +20,53 @@ cloudinary.config(
   api_secret =  os.getenv("CLOUDINARY_API_SECRET"),
 )
 
+FILENAME = 're'
+FILE_SRC_LOCATION =  '../scraper/output/usa_gundam/pass-5-manual-updates/%s.json'
+OUTPUT_LOCATION   =  './output/%s.json'
+
+
 def main():
   print('top of main')
-  ## POC WORK
-  res = cloudinary.uploader.upload(
-    "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-    public_id =  "olympic_flag",
-    tags      =  ["foo", "bar"])
+  # CONSIDERATIONS
+    # HOW TO SET FOLDER DEPTH
+      # grade/kit/file
 
-  print(res)
+  foo = get_kit_file(FILE_SRC_LOCATION % FILENAME)
+  # print(foo)
+  # print(type(foo))
+  for kit, data in foo.items():
+    print('data:', data)
+    if kit == 'other-kits': continue
+    counter = 0
+    for img in data['images']:
+      cloudinary_upload(img, public_id = f"kit-{counter}")
+      counter += 1
+    break
+
+  # open file  - ../scraper/output/usa_gundam/pass-5-manual-updates/re.json
+  # loop through dict of kit / value
+    # loop through images key
+      # wrap try/exception block here
+        # upload img to cloudinary
+        # save cloudinary['secure_url'] to kit['pics']
+      # exception
+        # save into errors file in ./output/errors.txt
+        # kit, scale
+  # open new file and save result  - ./output/<FILE_NAME>.json
+
   print('done')
+  return
+
+
+def get_kit_file(file_location:str):
+  with open(file_location, 'r+') as f:
+    file_data = json.load(f)
+  return file_data
+
+
+def cloudinary_upload(url:str, public_id: str, tags: List[str]=[]):
+  res = cloudinary.uploader.upload(url, public_id=public_id, tags=tags)
+  print(res['public_id'])
   return
 
 
